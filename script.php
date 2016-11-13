9129617506f9911c9167635b4c5c20f36143117a
Mike <mojemoron@yahoo.com>
2016-11-13 12:36:02 +0100

    added anew script.php to play with jquery

---
+++ script.php
@@ -0,0 +1,9 @@
+<?php require_once "include/header.php"?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-9">
+            Hello
+        </div>
+    </div>
+</div>
+<?php require_once "include/footer.php"?>
